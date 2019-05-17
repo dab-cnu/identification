92e08aa8039cc8819d5ce6365270ec9fbb373a3b
Junwon <rh3junwon@gmail.com>
2019-05-17 21:48:31 +0900

    add encrypt/decrypt func(dev WebDataController(2))

--- public/js/WebDataController.js
+++ public/js/WebDataController.js
@@ -1,27 +1,40 @@
 function addHuman() {
 	var _publicKey = document.getElementById("publicKeyForAdd").value;
+	var _privateKey = document.getElementById("privateKeyForAdd").value;
 	var _info = document.getElementById("info").value;
-	var _sign = document.getElementById("privateKey").value;
+	
+	var _encodedHumanInfo = encodeHumanInfo(_publicKey, _info);
+	var _sign = makeSign(_privateKey);
 
-	$.get("/addHuman?publicKey=" + _publicKey + "&info=" + _info + "&sign=" + _sign, function(data) {
+	$.post("/addHuman", { publicKey: sha256(_publicKey), info: _encodedHumanInfo, sign: _sign, function(data) {
 		if(data == "Error") {
 			$("#message").text("An error occured.");
 		}
 		else {
 			$("#message").html("Transaction hash: " + data);
 		}
-	});
+	}});
+
+	// $.get("/addHuman?publicKey=" + sha256(_publicKey) + "&info=" + _encodedHumanInfo + "&sign=" + _sign, function(data) {
+	// 	if(data == "Error") {
+	// 		$("#message").text("An error occured.");
+	// 	}
+	// 	else {
+	// 		$("#message").html("Transaction hash: " + data);
+	// 	}
+	// });
 }
 
 function getHuman() {
 	var _publicKey = document.getElementById("publicKeyForGet").value;
+	var _privateKey = document.getElementById("privateKeyForGet").value;
 
-	$.get("/getHuman?publicKey=" + _publicKey, function(data) {
+	$.get("/getHuman?publicKey=" + sha256(_publicKey), function(data) {
 		if (data[0] == "" && data[1] == "") {
 			$("#message").text("An error occured.");
 		}
 		else {
-			$("#message").html("info : " + data[0] + " sign : " + data[1]);
+			$("#message").html("info : " + decodeHumanInfo(_privateKey, data[0]) + " sign : " + verifySign(_publicKey, data[1]));
 		}
 	});
 }
