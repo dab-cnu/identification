45a364c3b772cb160dd2309749d2ad65cd926e7b
Junwon <rh3junwon@gmail.com>
2019-05-25 00:11:59 +0900

    add a contract

---
+++ contract/HumanInfo.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+
+contract HumanInfo {
+  address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  struct Human {
+    string info;
+    string sign;
+  }
+
+  mapping (string => Human) private humanList;
+
+  function addHuman(string memory _publicKey, string memory _info, string memory _sign) public {
+    // require(humanList[_publicKey]._sign == 0);
+    humanList[_publicKey] = Human(_info, _sign);
+  }
+
+  function getHuman(string memory _publicKey) public view returns (string memory _info, string memory _sign) {
+    // require(humanList[_account].age != 0);
+    _info = humanList[_publicKey].info;
+    _sign = humanList[_publicKey].sign;
+  }
+
+}
