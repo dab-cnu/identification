73b6dd3106710adc3b4c933ee2555d93aba543c7
woong <bsw0312009@cnu.ac.kr>
2019-07-11 01:08:28 +0900

    re fork

---
+++ dab/public/js/WebDataController.js
@@ -0,0 +1,61 @@
+function addHuman() {
+	var _publicKey = document.getElementById("publicKeyForAdd").value;
+	var _privateKey = document.getElementById("privateKeyForAdd").value;
+	var _info = document.getElementById("info").value;
+	
+	var _encodedHumanInfo = encodeHumanInfo(_publicKey, _info);
+	var _sign = makeSign(_privateKey);
+
+	$.post("/addHumanPage/addHuman", { publicKey: sha256(_publicKey), info: _encodedHumanInfo, sign: _sign}, function(data) {
+		if(data == "Error") {
+			$("#message").text("An error occured.");
+		}
+		else {
+			$("#message").html("Transaction hash: " + data + "encoded code : " + _encodedHumanInfo);
+
+		}
+	});
+}
+function addImg(){
+	var _img = document.getElementById("test").value;
+	//console.log(_img);
+	$.post("/addHumanPage/addImg", {image: _img}, function(data) {
+		if(data == "Error") {
+			$("#message").text("An error occured.");
+		}
+		else {
+			$("#message").html("image"+_img);
+		}
+	});
+}
+function getHuman() {
+	var _publicKey = document.getElementById("publicKeyForGet").value;
+	var _privateKey = document.getElementById("privateKeyForGet").value;
+
+	$.get("/getHumanPage/getHuman?publicKey=" + sha256(_publicKey), function(data) {
+		if (data[0] == "" && data[1] == "") {
+			$("#message").text("An error occured.");
+		}
+		else {
+			$("#message").html("info : " + decodeHumanInfo(_privateKey, data[0]));
+		}
+	});
+}
+
+function verify() {
+	var _publicKey = document.getElementById("publicKeyForGet").value;
+
+	$.get("/getHumanPage/getHuman?publicKey=" + sha256(_publicKey), function(data) {
+		if (data[0] == "" && data[1] == "") {
+			alert('falied...');
+		}
+		else {
+			if (verifySign(_publicKey, data[1])) {
+				alert('인증 확인되었습니다.');
+			}
+			else {
+				alert('인증 실패하였습니다.');
+			}
+		}
+	});
+}
---
+++ dab/public/js/encoder.js
@@ -0,0 +1,11 @@
+function encodeHumanInfo(_publicKey, _data) {
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(_publicKey);
+    return encrypt.encrypt(_data);
+}
+
+function makeSign(_privateKey) {
+    var sign = new JSEncrypt();
+    sign.setPrivateKey(_privateKey);
+    return sign.sign('ok', sha256, "sha256");
+}
