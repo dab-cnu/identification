310c29384ca91f2caba87d227cce26420c041cc7
Junwon <rh3junwon@gmail.com>
2019-07-16 14:49:52 +0900

    modify addHuman for image

--- ESC.js
+++ ESC.js
@@ -1,16 +1,15 @@
 var express = require("express");  
-var app = express();
 var bodyParser = require("body-parser");
-
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var server = require("http").createServer(app);
 
-server.listen(8080);
+server.listen(8085);
 
 console.log("Web server start!!");
-console.log("You can access the server (http://localhost:8080)");
+console.log("You can access the server (http://localhost:8085)");
 
 app.use(express.static("public"));
 
@@ -26,30 +25,49 @@ app.get("/getHuamnPage", function(req, res) {
 	res.sendFile(__dirname + "/public/html/getHumanPage.html");
 });
 
+
+
 var Web3 = require("web3");
 var fs = require('fs');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));	
 
-var contract = JSON.parse(fs.readFileSync('./HumanInfo.json', 'utf8'));
+var abi = JSON.parse(fs.readFileSync('./HumanInfo.json', 'utf8'));
 
-var humanInfoContract = web3.eth.contract(contract.abi);
-var humanInfo = humanInfoContract.at("0x4a21769ac327ff35c16ecb82527902036fef4634");
+var humanInfoContract = web3.eth.contract(abi);
+var humanInfo = humanInfoContract.at("0x053c00c2895b45835a81266a38358ae0a5d2953c");
 
+web3.eth.defaultAccount = web3.eth.accounts[0];
 
 // callAddHuman
 app.post("/addHumanPage/addHuman", function(req, res) {
-    var _publicKey = req.body.publicKey;
+	var _publicKey = req.body.publicKey;
     var _info = req.body.info;
-    var _sign = req.body.sign;
+	var _sign = req.body.sign;
+	var _image = 'test';
 
-		console.log('hi');
-    humanInfo.addHuman.sendTransaction(_publicKey, _info, _sign, {from: web3.eth.accounts[0]}, function(error, transactionHash) {
-		if (!error) {
-			res.send(transactionHash);
-		}
-		else {
-			res.send("Error");
-		}
+	var IPFS = require("ipfs")
+	var node = new IPFS({ start: false });
+
+	node.once('ready', () => {
+		node.add(Buffer.from(_image), (err, files) => {
+			if (err) return console.error(err)
+			var hashcode = files[0].hash
+
+			console.log('===================================');
+			console.log('[call addHuman]');
+			console.log('public Key (hash) : ' + _publicKey);
+			console.log('info (hash) : ' + _info);
+			console.log('sign (hash) : ' + _sign);
+			console.log('image (hash) : ' + hashcode);
+			console.log('-----------------------------------');
+			humanInfo.addHuman.sendTransaction(_publicKey, _info, {from: web3.eth.accounts[0], gas: web3.eth.estimateGas(humanInfo) + 150000});
+			console.log('function call : addHuman complete');
+			humanInfo.setSign.sendTransaction(_publicKey, _sign, {from: web3.eth.accounts[0], gas: web3.eth.estimateGas(humanInfo) + 150000});
+			console.log('function call : setSign complete');
+			humanInfo.setImage.sendTransaction(_publicKey, hashcode, {from: web3.eth.accounts[0], gas: web3.eth.estimateGas(humanInfo) + 150000});
+			console.log('function call : setImage complete');
+			console.log('===================================');
+		})
 	})
 });
 
